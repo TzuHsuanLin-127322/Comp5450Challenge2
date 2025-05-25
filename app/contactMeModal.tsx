f3b48c1a73c7d95d43b52095e876027aa30b0d8c
Tzu-Hsuan Lin (1273233) <tlin3@lakeheadu.ca>
2025-05-25 02:19:22 -0400

    Added preliminary version of contact me modal

--- app/contactMeModal.tsx
+++ app/contactMeModal.tsx
@@ -1,9 +1,104 @@
-import { Text, View } from "react-native";
+import { router } from "expo-router";
+import { useState } from "react";
+import { Button, ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
+
+const PURPOSE_OPTIONS = [
+  'Buy',
+  'Sell',
+  'Trade',
+  'Service',
+  'Other',
+];
 
 export default function ContactMeModal() {
-    return (
-        <View>
-            <Text>Contact Me</Text>
-        </View>
-    )
+  const [name, setName] = useState<string>('');
+  const [email, setEmail] = useState<string>('');
+  const [phone, setPhone] = useState<string>('');
+  const [purpose, setPurpose] = useState<number | null>(null);
+  const [message, setMessage] = useState<string>('');
+
+  return (
+    <ScrollView
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        padding: 16,
+      }}
+    >
+      <Text>Thank you for your interest in my services. Please fill out the form below to get in touch with me.</Text>
+      <Text style={{ fontSize: 16, fontWeight: 'bold', marginTop: 16 }}>Contact Information</Text>
+      <TextInput
+        placeholder="Name"
+        style={{
+          borderWidth: 1,
+          borderColor: 'gray',
+          padding: 8,
+          margin: 8,
+        }}
+      />
+      <TextInput
+        placeholder="Email"
+        style={{
+          borderWidth: 1,
+          borderColor: 'gray',
+          padding: 8,
+          margin: 8,
+        }}
+        keyboardType="email-address"
+      />
+      <TextInput
+        placeholder="Phone"
+        style={{
+          borderWidth: 1,
+          borderColor: 'gray',
+          padding: 8,
+          margin: 8,
+        }}
+        keyboardType="phone-pad"
+      />
+      <Text style={{ fontSize: 16, fontWeight: 'bold', marginTop: 16 }}>Purpose</Text>
+      <View style={{ flexDirection: 'row', flexWrap: 'wrap' }}>
+        {PURPOSE_OPTIONS.map((option, index) => (
+          <TouchableOpacity
+            style={{
+              alignItems: 'center',
+              justifyContent: 'center',
+              backgroundColor: 'white',
+              borderWidth: 2,
+              borderColor: index === purpose ? 'salmon' : 'lightgray',
+              borderRadius: 100,
+              minWidth: 56,
+              padding: 8,
+              margin: 8,
+              elevation: 4
+            }}
+            key={index}
+            onPress={() => setPurpose(index)}
+          >
+            <Text>{option}</Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+
+      <Text style={{ fontSize: 16, fontWeight: 'bold', marginTop: 16 }}>Message</Text>
+      <TextInput
+        placeholder="Message"
+        style={{
+          borderWidth: 1,
+          borderColor: 'gray',
+          padding: 8,
+          margin: 8,
+          minHeight: 100,
+          textAlignVertical: 'top',
+        }}
+        multiline={true}
+      />
+      <Button
+        title="Submit"
+        onPress={() => {
+          router.back()
+        }}
+      />
+    </ScrollView>
+  )
 }
