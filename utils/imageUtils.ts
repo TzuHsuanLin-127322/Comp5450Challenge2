9c2f7952acab2f951e68c4c3867e63c58dfb0483
Tianhua Lian <tlian1@lakeheadu.ca>
2025-05-26 22:24:30 -0400

    Update current deal to use imageUtils

--- utils/imageUtils.ts
+++ utils/imageUtils.ts
@@ -6,3 +6,16 @@ export const aboutMeCarImage: Record<string, ImageSourcePropType> = {
     'car3.jpeg': require('@/assets/images/car3.jpeg'),
     'car4.jpeg': require('@/assets/images/car4.jpeg'),
 }
+
+export const currentDealsCarImage: Record<string, ImageSourcePropType> = {
+    'BMWM5E60.png': require('@/assets/images/cars/BMWM5E60.png'),
+    'FordMustang.png': require('@/assets/images/cars/FordMustang.png'),
+    '2023TeslaModelS.jpg': require('@/assets/images/cars/2023TeslaModelS.jpg'),
+    'ToyotaCamry.png': require('@/assets/images/cars/ToyotaCamry.png'),
+    'Porsche911.png': require('@/assets/images/cars/Porsche911.png'),
+    'HondaCivicFD.png': require('@/assets/images/cars/HondaCivicFD.png'),
+    'ChevroletCamaro.png': require('@/assets/images/cars/ChevroletCamaro.png'),
+    'Mercedes-BenzS-Class.png': require('@/assets/images/cars/Mercedes-BenzS-Class.png'),
+    'AudiE-tron.png': require('@/assets/images/cars/AudiE-tron.png'),
+    'AudiE-JeepWranglerRubicon.png': require('@/assets/images/cars/JeepWranglerRubicon.png'),
+}
